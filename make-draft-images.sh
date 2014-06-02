68f09fafde7ff415699c0997ec86f514cd9eddc0
Emperor <valentine.emperor@gmail.com>
2014-06-02 14:03:35 +0400

    сделаны чертежи

--- make-draft-images.sh
+++ make-draft-images.sh
@@ -3,3 +3,15 @@
 echo 'converting draft 1...'
 /usr/bin/convert -density 300 draft-1.pdf -quality 90 drafts/draft-1.png
 echo 'finished converting draft 1'
+
+echo 'converting draft 2...'
+/usr/bin/convert -density 300 draft-2.pdf -quality 90 drafts/draft-2.png
+echo 'finished converting draft 2'
+
+echo 'converting draft 3...'
+/usr/bin/convert -density 300 draft-3.pdf -quality 90 drafts/draft-3.png
+echo 'finished converting draft 3'
+
+echo 'converting draft 4...'
+/usr/bin/convert -density 300 draft-4.pdf -quality 90 drafts/draft-4.png
+echo 'finished converting draft 4'
